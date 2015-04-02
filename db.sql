ac93c50904b873956fb69c31d2338334db84b3d7
user <spencer.wilson@colorado.edu>
2015-04-02 02:19:07 -0600

    updated the database

--- db.sql
+++ db.sql
@@ -13,21 +13,21 @@ CREATE TABLE IF NOT EXISTS `Source`(
 `SourceType` varchar (20) NOT NULL,
 `Reference` varchar(50) NOT NULL,
 PRIMARY KEY(`Id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 auto_increment=5;
+) ENGINE = MyISAM DEFAULT CHARSET = utf8 auto_increment=3;
 INSERT INTO `Source`(`Id`,`Name`,`SourceType`, `Reference`) VALUES
 	(1, 'Beeradvocate', 'web', 'http://www.beeradvocate.com/beer/style/'),
 	(2, 'BeerMe', 'primary source', 'from the people who brought you BeerMe');
 
-CREATE TABLE IF NOT EXISTS `Beer` (
+CREATE TABLE IF NOT EXISTS `Beers` (
 `Id` int(1) NOT NULL auto_increment,
-`Name` varchar(20) NOT NULL,
+`Name` varchar(30) NOT NULL,
 `BeerCatId` int(1) NOT NULL,
 `SourceId` int(1) NOT NULL,
-`abv` varchar(10) NOT NULL,
-`description` varchar(300) NOT NULL,
+`abv` varchar(30) NOT NULL,
+`description` varchar(500) NOT NULL,
 PRIMARY KEY(`Id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 auto_increment = 4 ;
-INSERT INTO `Beer` (`Id`,`Name`,`BeerCatId`,`SourceId`,`abv`,`description`) VALUES
+) ENGINE = MyISAM DEFAULT CHARSET = utf8 auto_increment = 4;
+INSERT INTO `Beers` (`Id`,`Name`,`BeerCatId`,`SourceId`,`abv`,`description`) VALUES
 	(1,'American Pale Ale (APA)', 1,1, '4.0-7.0%', 'Of British origin, this style is now popular worldwide and the use of local ingredients, or imported, produces variances in character from region to region. Generally, expect a good balance of malt and hops. Fruity esters and diacetyl can vary from none to moderate, and bitterness can range from lightly floral to pungent. American versions tend to be cleaner and hoppier, while British tend to be more malty, buttery, aromatic and balanced.'),
 	(2,'Imperial Pilsner',2,1,'6.5-9.0%','Similar to a Pilsner in appearance, but expect a more pronounced malty backbone and an intense bitterness. Malt flavors tend to be quite sweet in many examples. Alcohol can be quite aggressive and lend some spicy notes to the flavor.'),
 	(3,'Light Lager',2,1,'2.5-5.0%','The Light Lager is generally a lighter version of a breweries premium lager, some are lower in alcohol but all are lower in calories and carbohydrates compared to other beers. Typically a high amount of cereal adjuncts like rice or corn are used to help lighten the beer as much as possible. Very low in malt flavor with a light and dry body. The hop character is low and should only balance with no signs of flavor or aroma. European versions are about half the alcohol (2.5-3.5% abv) as their regular beer yet show more flavor (some use 100% malt) then the American counterparts. For the most part this style has the least amount of flavor than any other style of beer.');
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `FoodPairing`(
 `FoodId` int(1) NOT NULL,
 `ColorId` int(1) NOT NULL,
 `SourceId` int(1) NOT NULL,
-`Review` varchar (300) NOT NULL,
+`Review` varchar (400) NOT NULL,
 PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
 INSERT INTO `FoodPairing`(`Id`,`Name`,`BeerId`,`FoodId`,`ColorId`,`SourceId`,`Review`) VALUES
